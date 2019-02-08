34cc46fe28b40f275baf74a3796e808feaa111ff
Steve Johnson <steve@steveasleep.com>
2019-02-07 20:35:24 -0800

    Updates

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -56010,7 +56010,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63998" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60707" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
