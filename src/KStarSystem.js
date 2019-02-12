9ce5e059f32a75ea1dabc8e5ff2ce65c09d423c5
Steve Johnson <steve@steveasleep.com>
2019-02-11 19:50:10 -0800

    Better star name generator

--- src/KStarSystem.js
+++ src/KStarSystem.js
@@ -1,13 +1,22 @@
-import Chance from 'chance';
+import starnames from './starnames';
 import Alea from 'alea';
 import { StarSystem } from "stellardream";
+
+function patchingMathDotRandom(fn, code) {
+  const oldMR = Math.random;
+  Math.random = fn;
+  code();
+  Math.random = oldMR;  
+}
 
 export default class KStarSystem {
   constructor(seed) {
     this.seed = seed || Date.now();
-    this.chance = new Chance(this.seed);
-    this.name = this.chance.capitalize(this.chance.word({syllables: 4}));
     this.starSystem = new StarSystem(this.seed);
     this.alea = new Alea(this.seed);
+
+    patchingMathDotRandom(this.alea, () => {
+      this.name = starnames.flatten('#starname#');
+    });
   }
 }
